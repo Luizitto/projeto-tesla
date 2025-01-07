653021f5b3f5268f9d83eae59e2ce1024188df95
luizitto <luizvilela456@gmail.com>
2025-01-07 18:57:03 -0300

    projeto-tesla

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let formulario = document.querySelector(".formulario")
+console.log (formulario)
+
+
+function aparecerFormulario() {
+    formulario.style.left = "30%"
+
+}
+
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+
+}
+
+function aparecer2formulario(){
+
+}
+
+function desaparecer2formulario(){
+    
+}
